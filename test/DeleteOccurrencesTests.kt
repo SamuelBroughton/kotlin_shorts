51618c751cbb96c6d78dfb8817c03282f30bb2b8
SamuelBroughton <broughtonjsam@gmail.com>
2019-03-27 15:51:14 +0800

    Added tests to DeleteOccurrences

--- test/DeleteOccurrencesTests.kt
+++ test/DeleteOccurrencesTests.kt
@@ -4,15 +4,31 @@ import org.junit.jupiter.api.Assertions.assertArrayEquals
 
 class DeleteOccurrencesTests {
     @Test
-    fun `return empty intArray if maxOccurrences = 0`() {
+    fun `return empty intArray if maxOccurrences is less than 1`() {
         assertArrayEquals(
             intArrayOf(),
             deleteOccurrences(intArrayOf(23, 12, 23, 12, 23), 0)
         )
+        assertArrayEquals(
+            intArrayOf(),
+            deleteOccurrences(intArrayOf(1, 1, 1, 2, 2, 1, 1), -1)
+        )
     }
 
     @Test
-    fun `return expected number of max occurrences`() {
+    fun `return same intArray as entered when maxOccurences is greater than all occurrences`() {
+        assertArrayEquals(
+            intArrayOf(1, 2, 3, 1, 1, 2, 2, 3, 3, 4, 5),
+            deleteOccurrences(intArrayOf(1, 2, 3, 1, 1, 2, 2, 3, 3, 4, 5), 20)
+        )
+        assertArrayEquals(
+            intArrayOf(),
+            deleteOccurrences(intArrayOf(), 5)
+        )
+    }
+
+    @Test
+    fun `return intArray with expected number occuring elements`() {
         assertArrayEquals(
             intArrayOf(20, 37, 21),
             deleteOccurrences(intArrayOf(20, 37, 20, 21), 1)
@@ -28,10 +44,6 @@ class DeleteOccurrencesTests {
         assertArrayEquals(
             intArrayOf(1, 1, 1, 1, 1),
             deleteOccurrences(intArrayOf(1, 1, 1, 1, 1), 5)
-        )
-        assertArrayEquals(
-            intArrayOf(),
-            deleteOccurrences(intArrayOf(), 5)
         )
     }
 }
